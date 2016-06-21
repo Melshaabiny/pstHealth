95da9d8a506035c250299d7086aa5e27f77dbc49
Moustafa Elshaabiny <moustafa.elshaabiny@gmail.com>
2016-06-21 05:27:03 -0400

    removed db

--- pstHealth/settings.py
+++ pstHealth/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 from django.core.urlresolvers import reverse_lazy
 import os
+# Update database configuration with $DATABASE_URL.
 
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
